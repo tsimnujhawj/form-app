7d9d5d4d5f8922ee95f419c2616b31761fb39c6a
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-03-28 23:21:25 -0500

    refactored dir for server setup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-      <div>
           <h1>form-app</h1>
-      </div>
     )
   }
 }
