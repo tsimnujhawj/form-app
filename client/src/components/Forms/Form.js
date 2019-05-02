263a377c3ee4ef3b39550f1ee72014008443e98f
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-05-02 11:49:27 -0500

    update

--- client/src/components/Forms/Form.js
+++ client/src/components/Forms/Form.js
@@ -41,7 +41,7 @@ export default class Form extends Component {
         }
         const bodyFormData = new FormData()
         bodyFormData.append("name", "test")
-        // TODO: check syntax
+        // do we need axios?...
         axios({
             method: "POST",
             url: "https://my-json-server.typicode.com/tsimnujhawj/form-app",
